0c40ecae5256490be6c7abd4263af4043b8bf52d
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-09-20 14:04:22 +0530

    completed

--- machine_coding_ques/src/App.jsx
+++ machine_coding_ques/src/App.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProgressBar from "./components/ProgressBar";
 
 const App = () => {
+  const [val, setVal] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setVal((preVal) => preVal + 1);
+    }, 100);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+
   return (
     <div className="App">
       <span>Progress Bar</span>
-      <ProgressBar />
+      <ProgressBar value={val} />
     </div>
   );
 };
--- machine_coding_ques/src/components/ProgressBar.jsx
+++ machine_coding_ques/src/components/ProgressBar.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+const ProgressBar = ({ value = 0 }) => {
+  if (value < 0) {
+    value = 0;
+  } else if (value > 100) {
+    value = 100;
+  }
 
-const ProgressBar = () => {
-  return <div>ProgressBar</div>;
+  return (
+    <>
+      <div className="progress">
+        <span style={{ color: `${value >= 50 ? "#fff" : "#000"}` }}>
+          {value.toFixed()}%
+        </span>
+        <div style={{ width: `${value}%` }} />
+      </div>
+    </>
+  );
 };
 
 export default ProgressBar;
